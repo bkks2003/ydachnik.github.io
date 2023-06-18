a3759ea63f515d57d78e7e8e3cb15351e898b3bc
bkks2003 <96036373+bkks2003@users.noreply.github.com>
2023-06-18 20:34:43 +0600

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,13 +9,23 @@
 /******/ (function() { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
+/***/ "./src/assets/js/close__burger.js":
+/*!****************************************!*\
+  !*** ./src/assets/js/close__burger.js ***!
+  \****************************************/
+/***/ (function() {
+
+eval("\r\n\r\n\n\n//# sourceURL=webpack://brainscloud/./src/assets/js/close__burger.js?");
+
+/***/ }),
+
 /***/ "./src/assets/js/menu__scroll.js":
 /*!***************************************!*\
   !*** ./src/assets/js/menu__scroll.js ***!
   \***************************************/
 /***/ (function() {
 
-eval("// Прокрут к блоку с каталогом\r\nconst anchorsCat = document.querySelectorAll('a[href*=\"#catalog\"]')\r\n\r\nfor (let anchorCat of anchorsCat) {\r\n    anchorCat.addEventListener('click', function (eCat) {\r\n        eCat.preventDefault()\r\n\r\n        const blockIDCat = anchorCat.getAttribute('href').substr(1)\r\n\r\n        document.getElementById(blockIDCat).scrollIntoView({\r\n            behavior: 'smooth',\r\n            block: 'start'\r\n        })\r\n    })\r\n}\r\n\r\n// Прокрут к блоку с информацией\r\nconst anchorsAbout = document.querySelectorAll('a[href*=\"#about\"]')\r\n\r\nfor (let anchorAbout of anchorsAbout) {\r\n    anchorAbout.addEventListener('click', function (eAbout) {\r\n        eAbout.preventDefault()\r\n\r\n        const blockIDAbout = anchorAbout.getAttribute('href').substr(1)\r\n\r\n        document.getElementById(blockIDAbout).scrollIntoView({\r\n            behavior: 'smooth',\r\n            block: 'start'\r\n        })\r\n    })\r\n}\r\n// Прокрут к блоку с контактами\r\nconst anchorsCont = document.querySelectorAll('a[href*=\"#contact\"]')\r\n\r\nfor (let anchorCont of anchorsCont) {\r\n    anchorCont.addEventListener('click', function (eCont) {\r\n        eCont.preventDefault()\r\n\r\n        const blockIDCont = anchorCont.getAttribute('href').substr(1)\r\n\r\n        document.getElementById(blockIDCont).scrollIntoView({\r\n            behavior: 'smooth',\r\n            block: 'start'\r\n        })\r\n    })\r\n}\r\n// Прокрут к блоку с рекомендациями\r\nconst anchorsRec = document.querySelectorAll('a[href*=\"#recommendations\"]')\r\n\r\nfor (let anchorRec of anchorsRec) {\r\n    anchorRec.addEventListener('click', function (eRec) {\r\n        eRec.preventDefault()\r\n\r\n        const blockIDRec = anchorRec.getAttribute('href').substr(1)\r\n\r\n        document.getElementById(blockIDRec).scrollIntoView({\r\n            behavior: 'smooth',\r\n            block: 'start'\r\n        })\r\n    })\r\n}\r\n\n\n//# sourceURL=webpack://brainscloud/./src/assets/js/menu__scroll.js?");
+eval("// Прокрут к блоку с каталогом\r\nconst anchorsCat = document.querySelectorAll('a[href*=\"#catalog\"]')\r\n\r\nfor (let anchorCat of anchorsCat) {\r\n    anchorCat.addEventListener('click', function (eCat) {\r\n        eCat.preventDefault()\r\n\r\n        const blockIDCat = anchorCat.getAttribute('href').substr(1)\r\n\r\n        document.getElementById(blockIDCat).scrollIntoView({\r\n            behavior: 'smooth',\r\n            block: 'start'\r\n        })\r\n    })\r\n}\r\n\r\n// Прокрут к блоку с информацией\r\nconst anchorsAbout = document.querySelectorAll('a[href*=\"#about\"]')\r\n\r\nfor (let anchorAbout of anchorsAbout) {\r\n    anchorAbout.addEventListener('click', function (eAbout) {\r\n        eAbout.preventDefault()\r\n\r\n        const blockIDAbout = anchorAbout.getAttribute('href').substr(1)\r\n\r\n        document.getElementById(blockIDAbout).scrollIntoView({\r\n            behavior: 'smooth',\r\n            block: 'start'\r\n        })\r\n    })\r\n}\r\n// Прокрут к блоку с контактами\r\nconst anchorsCont = document.querySelectorAll('a[href*=\"#contact\"]')\r\n\r\nfor (let anchorCont of anchorsCont) {\r\n    anchorCont.addEventListener('click', function (eCont) {\r\n        eCont.preventDefault()\r\n\r\n        const blockIDCont = anchorCont.getAttribute('href').substr(1)\r\n\r\n        document.getElementById(blockIDCont).scrollIntoView({\r\n            behavior: 'smooth',\r\n            block: 'start'\r\n        })\r\n    })\r\n}\r\n// Прокрут к блоку с рекомендациями\r\nconst anchorsRec = document.querySelectorAll('a[href*=\"#recommendations\"]')\r\n\r\nfor (let anchorRec of anchorsRec) {\r\n    anchorRec.addEventListener('click', function (eRec) {\r\n        eRec.preventDefault()\r\n\r\n        const blockIDRec = anchorRec.getAttribute('href').substr(1)\r\n\r\n        document.getElementById(blockIDRec).scrollIntoView({\r\n            behavior: 'smooth',\r\n            block: 'start'\r\n        })\r\n    })\r\n}\r\n\r\n\n\n//# sourceURL=webpack://brainscloud/./src/assets/js/menu__scroll.js?");
 
 /***/ }),
 
@@ -35,6 +45,7 @@ eval("const btnUp = {\r\n    el: document.querySelector('.btn-up'),\r\n    show(
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	__webpack_modules__["./src/assets/js/close__burger.js"]();
 /******/ 	__webpack_modules__["./src/assets/js/menu__scroll.js"]();
 /******/ 	var __webpack_exports__ = {};
 /******/ 	__webpack_modules__["./src/assets/js/scroll__up.js"]();
